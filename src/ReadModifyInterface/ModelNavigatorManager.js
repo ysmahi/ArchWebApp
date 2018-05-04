1da4f209b0a8f07155c5907036452a7af1497719
Yazid Smahi <yazidsmahi@gmail.com>
2018-05-04 17:55:13 +0200

    Treeview now displays architecture of model & implementation of some github API calls

--- src/ReadModifyInterface/ModelNavigatorManager.js
+++ src/ReadModifyInterface/ModelNavigatorManager.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import {Treebeard} from 'react-treebeard';
+import _ from 'lodash';
+import { getBranchSha, getElementContent, getNodeTreeRecursive } from '../Utils/GithubApiCall'
+import { xml2json } from 'xml-js'
+
+// Used : http://brandonclapp.com/arranging-an-array-of-flat-paths-into-a-json-tree-like-structure/
 
 const data = {
   name: 'react-treebeard',
@@ -46,26 +51,114 @@ const data = {
   ]
 };
 
+let paths = [
+  'FolderA/FolderB/FolderC/Item1',
+  'FolderA/FolderB/Item1',
+  'FolderB/FolderD/FolderE/Item1',
+  'FolderB/FolderD/FolderE/Item2',
+  'FolderA/FolderF/Item1',
+  'ItemInRoot'
+];
+
 class ModelNavigatorManager extends React.Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {
+      dataNavigator: {
+        name: 'Architecture Model',
+        toggled: true,
+        children: []
+      }
+    }
     this.onToggle = this.onToggle.bind(this);
+
+    console.log('tree:', arrangeIntoTree(paths));
   }
-  onToggle(node, toggled){
+
+  componentDidMount() {
+    getBranchSha('ysmahi', 'ArchiTest', 'master')
+      .then((sha) => {
+        getNodeTreeRecursive('ysmahi', 'ArchiTest', sha)
+          .then((treeRecursive) => {
+            let allUrlsTree = treeRecursive.map((element) => element.path);
+            let tree = arrangeIntoTree(allUrlsTree);
+
+            let dataNavigator = {
+              name: 'Architecture Model',
+              toggled: true,
+              children: tree,
+            }
+
+            this.setState({dataNavigator: dataNavigator});
+          })
+      })
+  }
+
+  onToggle = (node, toggled) => {
+    // node.id est le lien du repo git
     if(this.state.cursor){this.state.cursor.active = false;}
     node.active = true;
     if(node.children){ node.toggled = toggled; }
     this.setState({ cursor: node });
+
+    if (!(node.children.length > 0)) {
+      // if a leaf is clicked
+      getElementContent('ysmahi', 'ArchiTest', node.id)
+        .then((content) =>
+        {
+          let contentElement = atob(content);
+          let jsonContentElement = xml2json(contentElement);
+          console.log(jsonContentElement);
+          // TODO on monday: pass this data to father component and then to Element Form to display data
+        })
+    }
   }
+
   render(){
     return (
       <Treebeard
-        data={data}
+        data={this.state.dataNavigator}
         onToggle={this.onToggle}
       />
     );
   }
+}
+
+let arrangeIntoTree = (paths) => {
+  let tree = [];
+
+  _.each(paths, (path) => {
+
+    let pathParts = path.split('/');
+
+    let currentLevel = tree; // initialize currentLevel to root
+
+    _.each(pathParts, (part) => {
+
+      // check to see if the path already exists.
+      let existingPath = _.find(currentLevel, {
+        name: part
+      });
+
+      if (existingPath) {
+        // The path to this item was already in the tree, so don't add it again.
+        // Set the current level to this path's children
+        currentLevel = existingPath.children;
+      }
+      else {
+        let newPart = {
+          name: part,
+          children: [],
+          id: path,
+        }
+
+        currentLevel.push(newPart);
+        currentLevel = newPart.children;
+      }
+    });
+  });
+
+  return tree;
 }
 
 export default ModelNavigatorManager;
---
+++ src/Utils/GithubApiCall.js
@@ -0,0 +1,48 @@
+import axios from 'axios/index';
+
+const apiUrl = 'https://api.github.com/';
+const reposUrl = 'repos/';
+
+export let getBranchSha = (username, repoName, branchName) => {
+  return new Promise(
+    (resolve, reject) => {
+      axios.get(apiUrl + reposUrl + username + '/' + repoName + '/branches/' + branchName)
+        .then((response) => {
+          if(response.status === 200) {
+            let branchSha = response.data.commit.sha;
+            resolve(branchSha);
+            reject('Error in getBranchSha');
+          }
+        })
+    }
+  )
+}
+
+export let getNodeTreeRecursive = (username, repoName, BranchSha) => {
+  return new Promise(
+    (resolve, reject) => {
+      axios.get(apiUrl + reposUrl + username + '/' + repoName + '/git/trees/' + BranchSha + '?recursive=1')
+        .then((response) => {
+          if(response.status === 200) {
+            resolve(response.data.tree);
+            reject('Error in getNodeTreeRecursive');
+          }
+        })
+    }
+  )
+}
+
+export let getElementContent = (username, repoName, pathElement) => {
+  return new Promise(
+    (resolve, reject) => {
+      axios.get(apiUrl + reposUrl + username + '/' + repoName + '/contents/' + pathElement)
+        .then((response) => {
+          if (response.status === 200)
+          {
+            resolve(response.data.content);
+            reject('Error in getElementContent');
+          }
+        })
+    }
+  )
+}
